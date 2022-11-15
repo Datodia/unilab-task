45023bf712256fa7c429f5ca72a25fd214f3f518
Datodia <dato.diasamidze.02@gmail.com>
2022-11-15 11:34:12 +0400

    feat: add localstorage functionality

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,15 +2,14 @@ import React, { useEffect } from 'react'
 import styles from './TodoList.module.css'
 
 import { useState } from "react";
+import useLocalStorage from '../../hooks/UseLocalStorage/useLocalStorage';
 
 export const TodoList = () => {
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useLocalStorage("todos", []);
     const [newTask, setNewTask] = useState("");
-    const [test, setTest] = useState([])
 
     const handleChange = (event) => {
         setNewTask(event.target.value);
-
     };
 
     const addList = () => {
@@ -20,13 +19,8 @@ export const TodoList = () => {
             completed: false
         };
         setTodoList([...todoList, task]);
-        localStorage.setItem("todos", JSON.stringify(todoList))
     };
 
-    // useEffect(() => {
-    //     localStorage.getItem("todos")
-    //     console.log(test)
-    // }, [setTest])
 
     const deleteTask = (id) => {
         setTodoList(todoList.filter((task) => task.id !== id));
@@ -50,12 +44,12 @@ export const TodoList = () => {
                 <input className={styles.input} onChange={handleChange} placeholder="my task" />
                 <button className={styles.button} onClick={addList}>ADD</button>
             </div>
+
             <div className={styles.taskList}>
                 {todoList.map((task) => {
                     return (
                         <div className={styles.tasks} style={{ background: task.completed ? "#61d885" : "black" }}>
                             <h1 className={styles.taskTitle}>{task.taskName}</h1>
-                            {/* <h1 style={{ color: 'white' }}>{localStorage.getItem('todos')}</h1> */}
                             <div className={styles.buttons}>
                                 <button className={styles.accept} onClick={() => compeleTask(task.id)}><img src='assets/done.svg' /></button>
                                 <button className={styles.delete} onClick={() => deleteTask(task.id)}> <img src='assets/delete.svg' /> </button>
